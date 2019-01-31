16eb54af4470dbbb6470e66a241487bfdeea554e
lance13c <pianoman13c@gmail.com>
2019-01-31 01:02:14 -0800

    uncommented tests

--- test/AnimeBuilderTest.js
+++ test/AnimeBuilderTest.js
@@ -35,43 +35,43 @@ describe("add", () => {
   });
   
 
-  // it("should add anime properties when no properties exist", () => {
+  it("should add anime properties when no properties exist", () => {
     
-  //   animeBuilder.add(SIMPLE_RULE);
+    animeBuilder.add(SIMPLE_RULE);
 
-  //   expect(animeBuilder.extractAnimeRules()).to.eql({
-  //      animeBuilderId: true,
-  //      translateX: [
-  //       {
-  //         value: 1,
-  //         duration: 100,
-  //         easing: "easeInOutQuad"
-  //       }
-  //     ]
-  //   });
-  // });
+    expect(animeBuilder.extractAnimeRules()).to.eql({
+       animeBuilderId: true,
+       translateX: [
+        {
+          value: 1,
+          duration: 100,
+          easing: "easeInOutQuad"
+        }
+      ]
+    });
+  });
 
-  // it("should append anime properties arrays on pre-existing properties", () => {
+  it("should append anime properties arrays on pre-existing properties", () => {
 
-  //   animeBuilder.add(SIMPLE_RULE);
-  //   animeBuilder.add(SIMPLE_RULE);
+    animeBuilder.add(SIMPLE_RULE);
+    animeBuilder.add(SIMPLE_RULE);
 
-  //   expect(animeBuilder.extractAnimeRules()).to.eql({
-  //      animeBuilderId: true,
-  //      translateX: [
-  //       {
-  //         value: 1,
-  //         duration: 100,
-  //         easing: "easeInOutQuad"
-  //       },
-  //       {
-  //         value: 1,
-  //         duration: 100,
-  //         easing: "easeInOutQuad"
-  //       }
-  //     ]
-  //   });
-  // });
+    expect(animeBuilder.extractAnimeRules()).to.eql({
+       animeBuilderId: true,
+       translateX: [
+        {
+          value: 1,
+          duration: 100,
+          easing: "easeInOutQuad"
+        },
+        {
+          value: 1,
+          duration: 100,
+          easing: "easeInOutQuad"
+        }
+      ]
+    });
+  });
 
   it("should throw an error when each property's duration sum does not equal one another", () => {
     const DIFFERENT_DURATION_RULE = {
@@ -94,34 +94,34 @@ describe("add", () => {
       expect(animeBuilder.add.bind(animeBuilder, DIFFERENT_DURATION_RULE)).to.throw(Error);
   });
 
-//   // placeholder-property-value is an object within the property array. An array is (translateX, translateY, scaleX, etc..)
-//   it("should add a placeholder-property-value to a non-existing property that's duration does not equal the totalDuration", () => {
+  // placeholder-property-value is an object within the property array. An array is (translateX, translateY, scaleX, etc..)
+  it("should add a placeholder-property-value to a non-existing property that's duration does not equal the totalDuration", () => {
     
-//     animeBuilder.add(SIMPLE_RULE);
-//     animeBuilder.add(SIMPLE_RULE_P2);
+    animeBuilder.add(SIMPLE_RULE);
+    animeBuilder.add(SIMPLE_RULE_P2);
     
-//     expect(animeBuilder.extractAnimeRules()).to.equal({
-//       animeBuilderId: true,
-//       translateX: [
-//         {
-//           value: 1,
-//           duration: 100,
-//           easing: "easeInOutQuad"
-//         }
-//       ],
-//       scaleX: [
-//         {
-//           value: '*=1',
-//           duration: 100,
-//         },
-//         {
-//           value: 1,
-//           duration: 300,
-//           easing: "easeInOutQuad"
-//         }
-//       ]
-//     });
-//   });
+    expect(animeBuilder.extractAnimeRules()).to.equal({
+      animeBuilderId: true,
+      translateX: [
+        {
+          value: 1,
+          duration: 100,
+          easing: "easeInOutQuad"
+        }
+      ],
+      scaleX: [
+        {
+          value: '*=1',
+          duration: 100,
+        },
+        {
+          value: 1,
+          duration: 300,
+          easing: "easeInOutQuad"
+        }
+      ]
+    });
+  });
 
 //   it("should add a placeholder-property-value to an existing property that's duration does not equal the totalDuration", () => {
 //     expect(true).to.equal(true);
